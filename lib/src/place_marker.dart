519961f50e7d9af598d26f088d22ee3434654355
Chiogros <alexandre.bouijoux@gmail.com>
2021-11-23 09:54:28 +0100

    PlaceMarker class which displays Places with different color depending place type

---
+++ lib/src/place_marker.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:parkedmap/src/place.dart';
+
+class PlaceMarker extends Marker {
+  PlaceMarker(Place place)
+  : super(
+    point: place.getLocation(),
+    builder: (_) {
+      Icon icon2display;
+      switch (place.getType()) {
+        case PlaceType.deg0:
+          icon2display = const Icon(
+            Icons.location_on,
+            size: 40,
+            color: Color(0xFF33AA33)
+          );
+          break;
+        case PlaceType.deg45:
+          icon2display = const Icon(
+              Icons.location_on,
+              size: 40,
+              color: Color(0xFFAA3333)
+          );
+          break;
+        case PlaceType.deg90:
+          icon2display = const Icon(
+              Icons.location_on,
+              size: 40,
+              color: Color(0xFF3333AA)
+          );
+          break;
+      }
+      return icon2display;
+    }
+  );
+}
